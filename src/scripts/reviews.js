b685dd3e4d9ac99909dfc642e1f4727fd3f708ee
Vadim Veksler <reyzele91@gmail.com>
2019-07-07 12:40:03 +0300

    .

---
+++ src/scripts/reviews.js
@@ -0,0 +1,52 @@
+import Vue from 'vue'
+import Flickity from 'vue-flickity'
+
+const review = {
+  template: '#review-template',
+  props: {
+    review: Object
+  }
+}
+
+new Vue({
+  el: '#reviews-slider',
+  template: '#reviews-template',
+  components: {
+    review,
+    Flickity
+  },
+  data: {
+    reviews: [],
+    flickityOptions: {
+      initialIndex: 0,
+      prevNextButtons: false,
+      pageDots: false,
+      wrapAround: true,
+      groupCells: true
+    }
+  },
+  methods: {
+    makeArrWithRequiredImages(data) {
+      return data.map((item) => {
+        const requiredPic = require(`../images/content/${item.avatar}`)
+        item.avatar = requiredPic
+
+        return item
+      })
+    },
+    next() {
+      this.$refs.flickity.next()
+    },
+
+    previous() {
+      this.$refs.flickity.previous()
+    }
+  },
+  created() {
+    const data = require('../data/reviews.json')
+    this.reviews = this.makeArrWithRequiredImages(data)
+  },
+  mounted() {
+    console.log(this.reviews)
+  }
+})
